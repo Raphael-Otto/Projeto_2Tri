74fcea11c0ae33a95c69724fb95a0edc97f32fc9
Raphis <101464708+Raphael-Otto@users.noreply.github.com>
2022-07-18 09:18:46 -0300

    Add files via upload

---
+++ Projeto_2Tri frontend/src/components/HeaderComp.vue
@@ -0,0 +1,83 @@
+<template>
+  <header>
+    <div class="logo">
+      <img src="@/assets/imgs/logo-removebg-preview.png" />
+    </div>
+    <div class="menu-central">
+      <span> <RouterLink to="/">Livros</RouterLink> </span>
+      <span> <RouterLink to="/">Editoras</RouterLink> </span>
+      <span> <RouterLink to="/">Autores</RouterLink> </span>
+      <span> <RouterLink to="/">Produtos</RouterLink> </span>
+    </div>
+    <div class="menu-direito">
+      <button @click="sair">SAIR</button>
+    </div>
+    <img class="img-usuario" src="@/assets/imgs/usuario.png" />
+  </header>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+
+header a {
+  padding: 0px 20px;
+  font-size: larger;
+}
+
+header {
+  margin: 0;
+  background-color: #616fee;
+  height: 15%;
+  font-size: 1rem;
+  padding-left: 2rem;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+/*---------------------------------*/
+
+.menu-central {
+  padding-left: 100px;
+  padding-right: 50px;
+}
+
+.menu-central span {
+  text-decoration: none;
+  font-family: 'Poppins' ;
+}
+  
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+/*---------------------------------*/
+
+.img-usuario {
+  margin-right: 10%;
+  padding-bottom: 7.5px;
+}
+
+.menu-direito button {
+  margin-left: 300px;
+  margin-right: 20px;
+  border-color: inherit;
+  background-color: #616fee;
+  border-radius: 30px;
+  border-width: 2px;
+  border-style: solid;
+  color: inherit;
+  font-weight: bolder;
+  font-size: small;
+  padding: 10px 25px 10px;
+  font-family: 'Poppins' ;
+}
+
+.menu-direito button:hover {
+  color: #616fee;
+  background: #FFFFFF;
+}
+
+</style>
---
+++ Projeto_2Tri frontend/src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('../views/HomeView.vue')
+    },
+    {
+      path: '/livros',
+      name: 'livros',
+      component: () => import('../views/LivrosView.vue')
+    },
+    {
+      path: '/editoras',
+      name: 'editoras',
+      component: () => import('../views/EditorasView.vue')
+    },
+    {
+      path: '/autores',
+      name: 'autores',
+      component: () => import('../views/AutoresView.vue')
+    },
+    {
+      path: '/produtos',
+      name: 'produtos',
+      component: () => import('../views/ProdutosView.vue')
+    },
+    {
+      path: '/cad_editoras',
+      name: 'cad_editoras',
+      component: () => import('../views/CadEditorasView.vue')
+    },
+  ]
+})
+
+export default router
